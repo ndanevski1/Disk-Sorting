95b34c774a5987111a0870c65ea20925f4b4f9d1
Vladimir Maksimovski <vlade.maksimovski@gmail.com>
2021-05-10 04:11:57 -0400

    Multi-attribute comparison

--- bsort.cc
+++ bsort.cc
@@ -26,15 +26,12 @@ vector<string> parse_tuple(string tuple){
 	values.push_back(attribute);
 	return values;
 }
-string get_key_attribute(string tuple, int sort_attribute_index) {
-	return parse_tuple(tuple)[sort_attribute_index];
-}
 
 int main(int argc, const char* argv[]) {
 
-	if (argc != 4) {
+	if (argc < 5) {
 		cout << "ERROR: invalid input parameters!" << endl;
-		cout << "Please enter <schema_file> <input_file> <out_index>" << endl;
+		cout << "Please enter <schema_file> <input_file> <out_index> <sorting_attributes>" << endl;
 		exit(1);
 	}
 	string schema_file(argv[1]);
@@ -44,12 +41,17 @@ int main(int argc, const char* argv[]) {
 	leveldb::DB *db;
 	leveldb::Options options;
 	options.create_if_missing = true;
+	options.error_if_exists = true;
 	leveldb::Status status = leveldb::DB::Open(options, out_index, &db);
 	if(!status.ok()){
 		cerr << status.ToString() << endl;
 		exit(-1);
 	}
-	vector<string> sort_attrs_name = {"start_year", "student_number"};
+	vector<string> sort_attrs_name;
+    for(int i = 4; i < argc; i++){
+        string sort_attr(argv[i]);
+        sort_attrs_name.push_back(sort_attr);
+    }
 	Schema schema = parse_schema(schema_file, sort_attrs_name);
 
 	ifstream input(input_file);
@@ -57,24 +59,26 @@ int main(int argc, const char* argv[]) {
 
 	long counter = 1;
 	for(string tuple; getline(input, tuple);) {
-		string key_s = get_key_attribute(tuple, schema.sort_attrs[0]) + "_" + to_string(counter);
+		string key_s = "";
+		vector<string> attributes = parse_tuple(tuple);
+		for(int i = 0; i < schema.sort_attrs.size(); i++){
+			key_s += attributes[schema.sort_attrs[i]];
+			key_s += "_";
+		}
+		key_s += to_string(counter);
 		counter++;
 
 		leveldb::Slice key = key_s;
 		leveldb::Slice value = tuple;
 
-		db->Put(leveldb::WriteOptions(), key, value); 
+		db->Put(leveldb::WriteOptions(), key, value);
 	}
 
 	leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
 	for (it->SeekToFirst(); it->Valid(); it->Next()) {
-		// leveldb::Slice key = it->key();
 		leveldb::Slice value = it->value();
-		// std::string key_str = key.ToString();
 		string val_str = value.ToString();
-		// cout << key_str << ": "  << val_str << endl;
 		output << val_str << endl;
-		// cout << val_str << endl;
 	}
 	assert(it->status().ok());  // Check for any errors found during the scan
 	delete it;
